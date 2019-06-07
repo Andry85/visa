aabba87415553612960e2f648ce8a3a5e461a8f1
Andry85 <shapovala@ukr.net>
2019-06-07 18:28:12 +0300

    remove updating wp and plugins

--- wp-content/themes/materialis/functions.php
+++ wp-content/themes/materialis/functions.php
@@ -59,3 +59,12 @@ if (version_compare(phpversion(), MATERIALIS_THEME_REQUIRED_PHP_VERSION, '>=')) 
 } else {
     add_action('admin_notices', 'materialis_php_version_notice');
 }
+
+remove_action('load-update-core.php','wp_update_themes');
+add_filter('pre_site_transient_update_themes',create_function('$a', "return null;"));
+wp_clear_scheduled_hook('wp_update_themes');
+add_filter('pre_site_transient_update_core',create_function('$a', "return null;"));
+wp_clear_scheduled_hook('wp_version_check');
+remove_action( 'load-update-core.php', 'wp_update_plugins' );
+add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
+wp_clear_scheduled_hook( 'wp_update_plugins' );
