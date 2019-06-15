67730f7ad7430aee9bd53d32948960bed08aaaea
Andry85 <shapovala@ukr.net>
2019-06-15 08:56:43 +0300

    fixed footer

--- wp-content/themes/materialis/footer.php
+++ wp-content/themes/materialis/footer.php
@@ -4,15 +4,15 @@ materialis_get_footer_content();
 wp_footer();
 ?>
 <div class="footer-text ">
-<p>ТОВ «Робота за кордоном від Андрія Марисюка» ЄДРПОУ: 42927728 © 2014-2019. Ліцензія №860 видана міністерством соціальної політики України. Всі права захищено.</p>
-<p class="footer__designed">Розроблено - <a href="https://prostosite.vn.ua/" title="створення сайтів">«ПростоСайт»</a></p>
+<p>Чешско-польский визовый центр 2012-2019.</p>
+<p class="footer__designed">Разработано - <a href="https://prostosite.vn.ua/" title="створення сайтів">«ПростоСайт»</a></p>
 </div>
 <div class="viberPopup">
 	<div class="viberPopup__shadow"></div>
 	<div class="viberPopup__inner">
 		<div class="viberPopup__cloze"></div>
-		<h2 class="viberPopup__title">Залишити відгук</h2>
-		<p class="viberPopup__text">Дякую, що співпрацювали з нами! <br>Для того, щоб залишити відгук - напишіть нам у Viber, <br>і ми з радістю його опублікуємо!</p>
+		<h2 class="viberPopup__title">Оставить отзыв</h2>
+		<p class="viberPopup__text">Спасибо, что сотрудничали с нами! <br>Для того, чтобы оставить отзыв - напишите нам в Viber, <br>и мы с радостью его опубликуем!</p>
 		<div class="viberPopup__viber"><span class="viber-icon"></span>+380955598344</div>
 	</div>
 	
