72d49a7a668e580aa334d93848045a14b9159aeb
Andry85 <shapovala@ukr.net>
2019-07-03 23:05:30 +0300

    added changes

--- wp-content/themes/materialis/footer.php
+++ wp-content/themes/materialis/footer.php
@@ -4,15 +4,15 @@ materialis_get_footer_content();
 wp_footer();
 ?>
 <div class="footer-text ">
-<p>Чешско-польский визовый центр 2012-2019.</p>
-<p class="footer__designed">Разработано - <a href="https://prostosite.vn.ua/" title="створення сайтів">«ПростоСайт»</a></p>
+<p>«Робота в Європі для своїх» 2012-2019.</p>
+<p class="footer__designed">Рорзроблено - <a href="https://prostosite.vn.ua/" title="створення сайтів">«ПростоСайт»</a></p>
 </div>
 <div class="viberPopup">
 	<div class="viberPopup__shadow"></div>
 	<div class="viberPopup__inner">
 		<div class="viberPopup__cloze"></div>
-		<h2 class="viberPopup__title">Оставить отзыв</h2>
-		<p class="viberPopup__text">Спасибо, что сотрудничали с нами! <br>Для того, чтобы оставить отзыв - напишите нам в Viber, <br>и мы с радостью его опубликуем!</p>
+		<h2 class="viberPopup__title">Залишити відгук</h2>
+		<p class="viberPopup__text">Спасибі, що співпрацювали з нами! <br>Для того, щоб залишити відгук - напишіть нам в Viber, <br>і ми з радістю його опублікуємо!</p>
 		<div class="viberPopup__viber"><span class="viber-icon"></span><a href="viber://chat?number=+380955598344">+380955598344</a></div>
 	</div>
 	
