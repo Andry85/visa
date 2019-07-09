203144e7c6285928942966788bb240fb755db483
Andry85 <shapovala@ukr.net>
2019-07-09 22:52:02 +0300

    added binotel

--- wp-content/themes/materialis/footer.php
+++ wp-content/themes/materialis/footer.php
@@ -18,5 +18,13 @@ wp_footer();
 	
 </div>
 
+<script type="text/javascript">
+  (function(d, w, s) {
+	var widgetHash = '998bdbgd3l771lyfejbz', gcw = d.createElement(s); gcw.type = 'text/javascript'; gcw.async = true;
+	gcw.src = '//widgets.binotel.com/getcall/widgets/'+ widgetHash +'.js';
+	var sn = d.getElementsByTagName(s)[0]; sn.parentNode.insertBefore(gcw, sn);
+  })(document, window, 'script');
+</script>
+
 </body>
 </html>
