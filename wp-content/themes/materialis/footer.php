723188cd2faa7d0bae2b4bc55143d2690f76081a
Andry85 <shapovala@ukr.net>
2019-07-10 20:59:45 +0300

    added a script

--- wp-content/themes/materialis/footer.php
+++ wp-content/themes/materialis/footer.php
@@ -25,6 +25,12 @@ wp_footer();
 	var sn = d.getElementsByTagName(s)[0]; sn.parentNode.insertBefore(gcw, sn);
   })(document, window, 'script');
 </script>
+<script>
+        (function(w,d,u){
+                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+        })(window,document,'https://cdn.bitrix24.ua/b7788533/crm/site_button/loader_1_yphpbd.js');
+</script>
 
 </body>
 </html>
