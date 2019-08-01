7eb6d9dfd9790ab8266f485f5b958ae2264cb81f
Andry85 <shapovala@ukr.net>
2019-08-01 23:42:13 +0300

    fixed footer

--- wp-content/themes/materialis/footer.php
+++ wp-content/themes/materialis/footer.php
@@ -13,7 +13,7 @@ wp_footer();
 		<div class="viberPopup__cloze"></div>
 		<h2 class="viberPopup__title">Залишити відгук</h2>
 		<p class="viberPopup__text">Спасибі, що співпрацювали з нами! <br>Для того, щоб залишити відгук - напишіть нам в Viber, <br>і ми з радістю його опублікуємо!</p>
-		<div class="viberPopup__viber"><span class="viber-icon"></span><a href="viber://add?number=+380505081732">+380505081732</a></div>
+		<div class="viberPopup__viber"><a href="viber://add?number=+380505081732"><span class="viber-icon"></span><i>0505081732</i></a></div>
 	</div>
 	
 </div>
