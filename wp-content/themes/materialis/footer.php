40d56e66ccb41a313bb990fd9161672b0b5c75e9
Andry85 <shapovala@ukr.net>
2019-08-19 22:28:06 +0300

    added viber

--- wp-content/themes/materialis/footer.php
+++ wp-content/themes/materialis/footer.php
@@ -13,7 +13,10 @@ wp_footer();
 		<div class="viberPopup__cloze"></div>
 		<h2 class="viberPopup__title">Залишити відгук</h2>
 		<p class="viberPopup__text">Спасибі, що співпрацювали з нами! <br>Для того, щоб залишити відгук - напишіть нам в Viber, <br>і ми з радістю його опублікуємо!</p>
-		<div class="viberPopup__viber"><a href="viber://chat?number=+380675375232"><span class="viber-icon"></span><i>0675375232</i></a></div>
+		<div class="viberPopup__viber viberPopup__viber--desktop"><a href="viber://chat?number=+380675375232"><span class="viber-icon"></span><i>0675375232</i></a></div>
+
+		<div class="viberPopup__viber viberPopup__viber--mobile"><a href="viber://add?number=380675375232"><span class="viber-icon"></span><i>0675375232</i></a></div>
+
 	</div>
 	
 </div>
